e6af64c6cb2cc7b12dbd7b5274c7210c117a04a9
johnhunsley <snowball33>
2018-03-27 17:08:49 +0100

    removed static data

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -32,49 +32,14 @@
     },
     data () {
       return {
-        nodes: [
-          { id: 1, name: 'my node 1', _color: '#59b5f2' },
-          { id: 2, name: 'my node 2', _color: 'orange' },
-          { id: 3, _color: 'orange' },
-          { id: 4, _color: 'orange' },
-          { id: 5, _color: '#59b5f2' },
-          { id: 6, _color: 'orange' },
-          { id: 7, _color: 'orange' },
-          { id: 8, _color: '#59b5f2' },
-          { id: 9, _color: '#59b5f2' },
-          { id: 10, _color: '#59b5f2' },
-          { id: 11, _color: 'orange' },
-          { id: 12, _color: '#59b5f2' },
-          { id: 13, _color: '#59b5f2' },
-          { id: 14, _color: '#59b5f2' }
-        ],
-        links: [
-          { sid: 1, tid: 2, _color: '#f25e59' },
-          { sid: 2, tid: 8, _color: '#f25e59' },
-          { sid: 3, tid: 4, _color: '#f25e59' },
-          { sid: 4, tid: 5, _color: '#90c6cc' },
-          { sid: 5, tid: 6, _color: '#90c6cc' },
-          { sid: 7, tid: 8, _color: '#90c6cc' },
-          { sid: 6, tid: 8, _color: '#90c6cc' },
-          { sid: 3, tid: 8, _color: '#f25e59' },
-          { sid: 7, tid: 9, _color: '#90c6cc' },
-          { sid: 5, tid: 10, _color: '#90c6cc' },
-          { sid: 10, tid: 11, _color: '#90c6cc' },
-          { sid: 4, tid: 11, _color: '#f25e59' },
-          { sid: 10, tid: 9, _color: '#90c6cc' },
-          { sid: 3, tid: 9, _color: '#90c6cc' },
-          { sid: 3, tid: 6, _color: '#90c6cc' },
-          { sid: 6, tid: 9, _color: '#90c6cc' },
-          { sid: 2, tid: 12, _color: '#90c6cc' },
-          { sid: 2, tid: 13, _color: '#90c6cc' },
-          { sid: 2, tid: 14, _color: '#90c6cc' }
-        ],
+        nodes: [],
+        links: [],
         options:
         {
           force: 3000,
           nodeSize: 10,
           nodeLabels: true,
-          linkWidth: 1,
+          linkWidth: 0.3,
           size: {
             w: 1130,
             h: 830
